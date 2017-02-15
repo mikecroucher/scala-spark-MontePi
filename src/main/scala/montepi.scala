e7706878bdd4c6a8ec057ccd02ca34b7c5fcd01f
Mike Croucher <michael.p.croucher@googlemail.com>
2017-02-15 01:51:47 +0000

    First commit

---
+++ src/main/scala/montepi.scala
@@ -0,0 +1,26 @@
+import org.apache.spark.sql.SparkSession
+// define main method (scala entry point)
+object montepi {
+  def main(args: Array[String]): Unit = {
+
+    // initialise spark session (running in "local" mode)
+    val sparkSession = SparkSession.builder
+      .master("local")
+      .appName("Hello World")
+       .getOrCreate()
+
+    val sc = sparkSession.sparkContext
+    val NUM_SAMPLES = 1000000000
+
+    val count = sc.parallelize(1 to NUM_SAMPLES).map{i =>
+    val x = Math.random()
+    val y = Math.random()
+    if (x*x + y*y < 1) 1 else 0
+}.reduce(_ + _)
+
+println("Pi is roughly " + 4.0 * count / NUM_SAMPLES)
+    
+    // terminate underlying spark context
+    sparkSession.stop()
+  }
+}
